5adeeca3696ca3f643f9dba8f89e2697d33eb7d7
c-orte-x <36413036+c-orte-x@users.noreply.github.com>
2019-08-17 17:54:30 +0200

    Copied newly created project with code for the booking layout

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ITRW324Project2.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
